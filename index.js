593be42c03802f5a3c25221b7aea26996e5f2fda
A-Farabi <farabisyl2017@gmail.com>
2024-09-23 23:53:36 +0600

    tab switch done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const donationTab = document.getElementById("donationTab")
 const historyTab = document.getElementById("historyTab")
+const donateSection = document.getElementById("donateSection")
 
 donationTab.addEventListener("click", function(){
     // donationTab.classList.remove('hidden')
@@ -7,10 +8,12 @@ donationTab.addEventListener("click", function(){
     // donationTab.classList.add('bg-white')
     donationTab.classList.add('bg-[#b4f461]')
     donationTab.classList.remove('bg-white')
+    donateSection.classList.remove('hidden')
 })
 
 historyTab.addEventListener("click", function(){
     donationTab.classList.remove('bg-[#b4f461]')
     historyTab.classList.remove('bg-white')
     historyTab.classList.add('bg-[#b4f461]')
+    donateSection.classList.add('hidden')
 })
